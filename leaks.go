90555ea2e710e2c184db5656e313b5580b7e6538
Brian Sizemore <bpsizemore@gmail.com>
2019-08-26 21:16:55 -0500

    initial stuff

---
+++ leaks.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"github.com/urfave/cli"
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/urface/cli"
+)
+
+type Result struct {
+	num_hints int
+}
+
+func search(query string) {
+
+	file, err := os.Open("./leaks.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		if strings.Contains(scanner.Text(), query) {
+			fmt.Println(scanner.Text())
+			//Eventually these should not be printed ad hoc.
+			//Result.append(scanner.Text())
+		}
+	}
+	//return Results
+}
+
+func main() {
+	var fuzzy bool
+	var query string
+	
+	app := cli.NewApp()
+
+	app.Flags = []cli.Flag {
+		cli.StringFlag{
+			Name: "query, q",
+			Value: "",
+			Usage: "String to search for",
+		},
+		cli.BoolFlag{
+			Name: "fuzzy",
+			value: false,
+			Usage: "keeping it fuzzy"
+		},
+	}
+
+	app.Name = "LeakSearch"
+	app.Usage = "Looks like you've got a leak..."
+	app.Action = func(c *cli.Context) error {
+		fmt.Println("Can't fix a leak without the write tools...")
+		return nil
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+	search(query)
+}
